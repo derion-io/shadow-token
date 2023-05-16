8fb8c8d9e7d6d743f513ba57b1c76d5e88eab3c9
Zergity <zergity@gmail.com>
2023-05-16 15:37:32 +0700

    remove duplicate dependencies

--- contracts/Shadow.sol
+++ contracts/Shadow.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@derivable/erc1155-timelock/contracts/token/ERC1155/IERC1155.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 
