a67bd5d59af3491c7b3b51c9c09367a308313121
Ngo Quang Anh <anhnq82@gmail.com>
2023-09-03 13:38:00 +0700

    configure evmVersion

--- hardhat.config.js
+++ hardhat.config.js
@@ -67,6 +67,7 @@ module.exports = {
   solidity: {
     version: argv.compiler,
     settings: {
+      evmVersion: 'paris',
       optimizer: {
         enabled: withOptimizations,
         runs: 200,
